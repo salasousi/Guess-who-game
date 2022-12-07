598de82e27ccde957b7b91fd08b3350887e03865
salasousi <102608394+salasousi@users.noreply.github.com>
2022-12-07 18:08:48 -0500

    Checks if user's input matches with shown player's name
    All previous guesses go into an array and user is able to press a button to select a new player to guess

--- project1/js/script.js
+++ project1/js/script.js
@@ -6,14 +6,15 @@ $(function(){
 const URL = "https://api.sportmonks.com/v3/football/players/countries/"
 const api_token = "?api_token=S4LIHhpkF0fLuxiJWoDhKW2kBESvCEMxwsADBC3rIUMseGsQ39AH3BCidtsu"
 let playerArray;
+let ranPlayer;
 let ranArr;
 const countryID = ["2", "5", "11", "17", "20", "32", "38", "44", "47",
  "62", "98", "107", "74505", "459", "200", "1739", "479", "488", "3483", "515", 
 "556", "1004", "1424", "266", "35376", "458", "296", "593", "320", "1439", "468", "158", "712"];
 let prevGuesses = [];
 let prevGuessesEl = document.getElementById("prevGuesses");
-const guessLimit = 10;
-const guessesLeftEl = document.getElementById("#guessesLeftDisplay");
+let guessLimit = 10;
+let guessesLeftEl = document.getElementById("#guessesLeftDisplay");
 
 
 //////////////////
@@ -21,15 +22,16 @@ const guessesLeftEl = document.getElementById("#guessesLeftDisplay");
 //////////////////
 const $form = $("form");
 const $input = $("input[type='text']");
+const $guessButn = $("#guess");
 const $position = $("position");
 const $playerStats = $("#player-stats");
-const $firstname = $("firstname")
-const $lastname = $("lastname")
+const $firstname = $("firstname");
+const $lastname = $("lastname");
 
 //////////////////
 // EVENT LISTENERS
 //////////////////
-$form.on("click", "submit", guessClick);
+$guessButn.on("click", guessClick);
 // $playerStats.on("click", handleClick)
 
 
@@ -67,30 +69,29 @@ getPlayerData();
     //  }
 
     function guessClick(event){
-    //     const $target = $(ranArr);
-    //     const answer = $target;
-    //     const $idk = $target.closest("main");
-    //     const playerIndex = $idk.attr("data-index");
-    //     const playerRef = ranArr[playerIndex];
-
         event.preventDefault();
         userInput= $("#text").val();
         prevGuesses.push(userInput);
+    
 
-        for (let i = guessLimit; i > 0; --i){
+        while (guessLimit > 0){
             prevGuessEl = prevGuesses.join(', ');
             console.log(prevGuesses);
-            $("#guessesLeft").html(i);
+            $("#guessesLeft").html(guessLimit);
         
-
-            if (userInput != checkName){
-                console.log("Try again")
-            } else{
-                console.log("Correct")
-            }
+            guessLimit -=1;
+          
         }
+        if (userInput != ranArr[0].fullname){
+            console.log("Try again")
+        } else {
+            console.log("Correct")
+        }
+        //console.log(ranArr[0].fullname)
 
     }
+
+    
    
 
     
@@ -109,8 +110,8 @@ function render(){
     });
     $playerStats.html(playerData)
     console.log(ranPlayer.fullname)
-    console.log(ranArr.fullname)
-    
+
+
     
 }
 {/* <h3>${player.position}</h3> 
